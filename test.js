9f8c41e812cbe6f19bf08e27c9294bd38d540c54
Alexander Kochergin <monsterlessons@gmail.com>
2017-04-01 20:33:27 +0200

    05 operators

--- test.js
+++ test.js
@@ -1,17 +1,3 @@
-var day
+var number1 = (4 + 3) * 5
 
-day = 1
-
-var basicValue = 12345
-
-var someText = 'Hello world'
-
-var gameOver = true
-
-var isLoggedIn = true
-
-var name = 'Alex'
-
-var isloggedin = true
-
-document.write(someText)
+document.write(number1)
