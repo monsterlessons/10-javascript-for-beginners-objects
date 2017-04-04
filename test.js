1d91d7619236ea998a05d83d83c49199a4a0bc65
Alexander Kochergin <monsterlessons@gmail.com>
2017-04-04 23:11:25 +0200

    06 conditionals

--- test.js
+++ test.js
@@ -1,3 +1,11 @@
-var number1 = (4 + 3) * 5
+var name = 'Mike'
 
-document.write(number1)
+if (name == 'Alex') {
+  document.write('My name is Alex')
+} else if (name == 'Mike') {
+  document.write('My name is Mike')
+} else if (name == 'John') {
+  document.write('My name is John') {    
+} else {
+  document.write('My name is not Alex')
+}
